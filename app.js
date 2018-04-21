a25b24740bd13fc3445f5a7db57a82c7a3f358c5
bigless27 <lessonryan@gmail.com>
2018-04-21 16:37:10 -0700

    complete project

--- app.js
+++ app.js
@@ -1,3 +1,57 @@
-$(document).ready(function() {
     console.log("page loaded");
-});
+
+    function getPokemon() {
+        $.get("pokemon/", (data) => {
+                $.each(data, function(index, val) {
+                    $("#content").append(`<div>${index + 1} ${val.name}</div>`);
+                });
+            })
+            .done(() => {
+                console.log("done");
+            })
+            .fail((err) => {
+                console.log(err);
+            });
+    }
+
+    function getOnePokemon() {
+        $.get("pokemon/1", (data) => {
+                $("#content").append(`<div>${index + 1} ${data.name}</div>`);
+            })
+            .done(() => {
+                console.log("done");
+            })
+            .fail((err) => {
+                console.log(err);
+            });
+    }
+
+    function createPokemon() {
+        let newPokemon = { "name": "Mew" };
+        $.post("pokemon", newPokemon, (data) => {
+            console.log(data);
+        });
+    }
+
+    function updatePokemon() {
+        $.ajax({
+            type: "PUT",
+            dataType: "json",
+            data: { "name": "Bulba" },
+            url: "pokemon/1",
+            success: function(data) {
+                alert(data);
+            }
+        });
+    }
+
+    function deletePokemon() {
+        $.ajax({
+            type: "DELETE",
+            dataType: "json",
+            url: "pokemon/1",
+            success: function(data) {
+                alert(data);
+            }
+        });
+    }
